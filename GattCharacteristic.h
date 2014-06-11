0c36eed3be3ddac163704630f8d22fa820c3f84f
Rohit Grover <rohit.grover@arm.com>
2014-06-11 08:32:18 +0100

    merge GattCharacteristic.cpp into the .hpp file

--- GattCharacteristic.h
+++ GattCharacteristic.h
@@ -302,7 +302,35 @@ public:
         uint16_t gatt_nsdesc;    /**< Namespace description from Bluetooth Assigned Numbers, normally '0', see @ref BLE_GATT_CPF_NAMESPACES. */
     } presentation_format_t;
 
-    GattCharacteristic(const UUID &uuid, uint16_t initialLen = 1, uint16_t maxLen = 1, uint8_t properties = 0);
+    /**
+     *  @brief  Creates a new GattCharacteristic using the specified 16-bit
+     *          UUID, value length, and properties
+     *
+     *  @note   The UUID value must be unique in the service and is normally >1
+     *
+     *  @param[in]  uuid
+     *              The UUID to use for this characteristic
+     *  @param[in]  initialLen
+     *              The min length in bytes of this characteristic's value
+     *  @param[in]  maxLen
+     *              The max length in bytes of this characteristic's value
+     *  @param[in]  props
+     *              The 8-bit bit field containing the characteristic's properties
+     *
+     *  @section EXAMPLE
+     *
+     *  @code
+     *
+     *  // UUID = 0x2A19, Min length 2, Max len = 2, Properties = write
+     *  GattCharacteristic c = GattCharacteristic( 0x2A19, 2, 2, BLE_GATT_CHAR_PROPERTIES_WRITE );
+     *
+     *  @endcode
+    */
+    /**************************************************************************/
+    GattCharacteristic(const UUID &uuid, uint16_t initialLen, uint16_t maxLen, uint8_t props) :
+        _uuid(uuid), _lenMin(initialLen), _lenMax(maxLen), _handle(), _properties(props) {
+        /* empty */
+    }
 
 public:
     uint16_t getHandle(void) const {
