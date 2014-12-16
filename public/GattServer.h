fedcae1c12fb2c4af38f0660d9b679ecb350d537
Rohit Grover <rohit.grover@arm.com>
2014-12-16 08:48:09 +0000

    minor white space diffs.

--- public/GattServer.h
+++ public/GattServer.h
@@ -29,17 +29,24 @@ public:
     typedef void (*ServerEventCallback_t)(void);                    /**< independent of any particular attribute */
 
 protected:
-    GattServer() : serviceCount(0), characteristicCount(0), onDataSent(), onDataWritten(), onUpdatesEnabled(NULL), onUpdatesDisabled(NULL), onConfirmationReceived(NULL) {
+    GattServer() :
+        serviceCount(0),
+        characteristicCount(0),
+        onDataSent(),
+        onDataWritten(),
+        onUpdatesEnabled(NULL),
+        onUpdatesDisabled(NULL),
+        onConfirmationReceived(NULL) {
         /* empty */
     }
 
     friend class BLEDevice;
 private:
     /* These functions must be defined in the sub-class */
-    virtual ble_error_t addService(GattService &) = 0;
+    virtual ble_error_t addService(GattService &)                                             = 0;
     virtual ble_error_t readValue(uint16_t handle, uint8_t buffer[], uint16_t *const lengthP) = 0;
-    virtual ble_error_t updateValue(uint16_t, uint8_t[], uint16_t, bool localOnly = false) = 0;
-    virtual ble_error_t initializeGATTDatabase(void) = 0;
+    virtual ble_error_t updateValue(uint16_t, uint8_t[], uint16_t, bool localOnly = false)    = 0;
+    virtual ble_error_t initializeGATTDatabase(void)                                          = 0;
 
     // ToDo: For updateValue, check the CCCD to see if the value we are
     // updating has the notify or indicate bits sent, and if BOTH are set
