f89fcb3ec2cdc51fd9c18178d9dbb402b0a2a07b
Rohit Grover <rohit.grover@arm.com>
2015-05-20 10:19:05 +0100

    remove extraneous declaration of AdvertisementReportCallback_t

--- public/Gap.h
+++ public/Gap.h
@@ -164,13 +164,6 @@ public:
     };
     typedef FunctionPointerWithContext<const AdvertisementCallbackParams_t *> AdvertisementReportCallback_t;
 
-    typedef void (*AdvertisementReportCallback_t)(const address_t      peerAddr,
-                                                  int8_t               rssi,
-                                                  bool                 isScanResponse,
-                                                  AdvertisementType_t  type,
-                                                  uint8_t              advertisingDataLen,
-                                                  const uint8_t       *advertisingData);
-
     friend class BLEDevice;
 
 private:
