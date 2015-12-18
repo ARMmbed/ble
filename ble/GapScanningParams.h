3be7e9ce13e5565501f2239427c4e2e95493e4c4
Rohit Grover <rohit.grover@arm.com>
2015-12-18 13:20:36 +0000

    GapScanningParams: add getter and setter for whitelist

--- ble/GapScanningParams.h
+++ ble/GapScanningParams.h
@@ -47,17 +47,50 @@ public:
 
 public:
     /* @Note: The following return durations in units of 0.625ms */
-    uint16_t getInterval(void) const {return _interval;}
-    uint16_t getWindow(void)   const {return _window;  }
+    uint16_t getInterval(void)       const {return _interval;      }
+    uint16_t getWindow(void)         const {return _window;        }
 
-    uint16_t getTimeout(void)  const {return _timeout; }
+    uint16_t getTimeout(void)        const {return _timeout;       }
     bool     getActiveScanning(void) const {return _activeScanning;}
 
+    /**
+     * Accessor for whiteList.
+     */
+    const BLEProtocol::Whitelist_t *getWhitelist(void) const {
+        return _whiteList;
+    }
+
+    /**
+     * Set the whiteList to be used for scanning. See @ref
+     * BLEProtocol::Whitelist_t. When used for a Central, advertisements are
+     * filtered by the white-list.
+     *
+     * @param [in] whiteList The white-list to be used for connections
+     *                 advertisements. If passed in as NULL, the existing
+     *                 whitelist (if any) remains in effect.
+     *
+     * @note The contents of the structure holding the white-list should remain
+     *     valid at least until scanning or a connection request is issued.
+     *
+     * @note It is not possible to change the contents of the whitelist while it
+     *     is being used. For example, if the device is waiting for a connection
+     *     and using the whitelist to filter the devices to and from which it
+     *     will respond, the whitelist cannot be changed until the
+     *     connection/scan request times out. The whitelist can then be changed
+     *     and scanning re-enabled.
+     *
+     * @note this API is experimental.
+     */
+    void setWhitelist(const BLEProtocol::Whitelist_t *whiteList) {
+        _whiteList = whiteList;
+    }
+
 private:
-    uint16_t _interval; /**< Scan interval in units of 625us (between 2.5ms and 10.24s). */
-    uint16_t _window;   /**< Scan window in units of 625us (between 2.5ms and 10.24s). */
-    uint16_t _timeout;  /**< Scan timeout between 0x0001 and 0xFFFF in seconds; 0x0000 disables timeout. */
-    bool     _activeScanning; /**< Obtain the peer device's advertising data and (if possible) scanResponse. */
+    uint16_t                        _interval;       /**< Scan interval in units of 625us (between 2.5ms and 10.24s). */
+    uint16_t                        _window;         /**< Scan window in units of 625us (between 2.5ms and 10.24s). */
+    uint16_t                        _timeout;        /**< Scan timeout between 0x0001 and 0xFFFF in seconds; 0x0000 disables timeout. */
+    bool                            _activeScanning; /**< Obtain the peer device's advertising data and (if possible) scanResponse. */
+    const BLEProtocol::Whitelist_t *_whiteList;      /**< white list associated with advertisements. \ref BLEProtocol::Whitelist_t */
 
 private:
     /* Disallow copy constructor. */
