566de1d706ecb939b1c69af4e34b25ed350544a8
Andres Amaya Garcia <Andres.AmayaGarcia@arm.com>
2016-01-22 17:05:14 +0000

    Add documentation to OneShot*Callback.h

--- ble/OneShotReadCallback.h
+++ ble/OneShotReadCallback.h
@@ -22,16 +22,55 @@
 #ifndef __ONESHOTREADCALLBACK_H__
 #define __ONESHOTREADCALLBACK_H__
 
+/**
+ * This class encapsulates the data needed to keep track a read procedure to
+ * a GATT Attribute in the peer's ATT table. The object can be instanciated by
+ * using the launch() function with the required arguments. The user is able to
+ * register a read callback that will be executed at the end of the read
+ * procedure with the final outcome of the operation.
+ *
+ * @note This object will automatically release any resources acquired during
+ *       its lifetime.
+ */
 class OneShotReadCallback {
 public:
+    /**
+     * Create a new instance of the object and launch the read procedure with
+     * the specified arguments.
+     *
+     * @param[in] client
+     *              A pointer to the GattClient used to trigger the read
+     *              procedure.
+     * @param[in] connHandle
+     *              The ID of the connection to execute the read procedure.
+     * @param[in] handle
+     *              The attribute handle of the value read in the peer's
+     *              ATT table.
+     * @param[in] cb
+     *              The callback executed at the end of the read procedure.
+     */
     static void launch(GattClient* client, Gap::Handle_t connHandle,
                        GattAttribute::Handle_t handle, const GattClient::ReadCallback_t& cb) {
         OneShotReadCallback* oneShot = new OneShotReadCallback(client, connHandle, handle, cb);
         oneShot->attach();
-        // delete will be made when this callback is called
+        /* Delete will be made when this callback is called */
     }
 
 private:
+    /**
+     * Construct a new instance of this object.
+     *
+     * @param[in] client
+     *              A pointer to the GattClient used to trigger the read
+     *              procedure.
+     * @param[in] connHandle
+     *              The ID of the connection to execute the read procedure.
+     * @param[in] handle
+     *              The attribute handle of the value read in the peer's
+     *              ATT table.
+     * @param[in] cb
+     *              The callback executed at the end of the read procedure.
+     */
     OneShotReadCallback(GattClient* client, Gap::Handle_t connHandle,
                         GattAttribute::Handle_t handle, const GattClient::ReadCallback_t& cb) :
         _client(client),
@@ -39,15 +78,30 @@ private:
         _handle(handle),
         _callback(cb) { }
 
+    /**
+     * Attach callbacks to the GattClient and Gap to receive shutdown, data
+     * read and disconnection events.
+     */
     void attach() {
         _client->onDataRead(makeFunctionPointer(this, &OneShotReadCallback::call));
         _client->onShutdown(this, &OneShotReadCallback::shutdownCallback);
         BLE::Instance().gap().onDisconnection(this, &OneShotReadCallback::disconnectionCallback);
     }
 
+    /**
+     * This callback will be executed in case the user calls BLE::shutdown()
+     * before the read procedure completes. In this case, the call() callback
+     * will never be executed, but the resources acquired by this object need
+     * to be released.
+     *
+     * @param[in] client
+     *              A pointer to the GattClient used to trigger the read
+     *              procedure.
+     */
     void shutdownCallback(const GattClient *client) {
         /* Avoid compiler warnings */
         (void) client;
+
         /*
          * Free resources allocated to this object. Pending read callbacks
          * will no longer be executed.
@@ -55,6 +109,15 @@ private:
         delete this;
     }
 
+    /**
+     * This callback will be executed when a disconnection even occurs before
+     * the read procedure completes. In this case, the call() callback
+     * will never be executed, but the resources acquired by this object need
+     * to be released.
+     *
+     * @param[in] params
+     *              The disconnection parameters.
+     */
     void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params) {
         if (params->handle == _connHandle) {
             /* Tell the user that the operation failed */
@@ -77,8 +140,20 @@ private:
         }
     }
 
+    /**
+     * Registered callback that will be executed at the end of the read
+     * procedure. If this callback is called with the correct connection and
+     * attribute handles, then it will do the following:
+     * - Call the user-registered read callback.
+     * - Detach its callbacks registered for disconnection, shutdown and
+     *   data read events.
+     * - Release its resources.
+     *
+     * @param[in] param
+     *              Parameters specifying the result of the write operation.
+     */
     void call(const GattReadCallbackParams* params) {
-        // verifiy that it is the right characteristic on the right connection
+        /* Verifiy that it is the right characteristic on the right connection */
         if (params->connHandle == _connHandle && params->handle == _handle) {
             _callback(params);
             _client->onDataRead().detach(makeFunctionPointer(this, &OneShotReadCallback::call));
@@ -88,9 +163,22 @@ private:
         }
     }
 
+    /**
+     * The GattClient instance that created this object.
+     */
     GattClient* _client;
+    /**
+     * The connection ID that used to execute the read procedure.
+     */
     Gap::Handle_t _connHandle;
+    /**
+     * The attribute handle of the value read in the peer's ATT table.
+     */
     GattAttribute::Handle_t _handle;
+    /**
+     * The registered callback that will be executed when the read procedure
+     * terminates.
+     */
     GattClient::ReadCallback_t _callback;
 };
 
--- ble/OneShotWriteCallback.h
+++ ble/OneShotWriteCallback.h
@@ -22,16 +22,55 @@
 #ifndef __ONESHOTWRITECALLBACK_H__
 #define __ONESHOTWRITECALLBACK_H__
 
+/**
+ * This class encapsulates the data needed to keep track a write procedure to
+ * a GATT Attribute in the peer's ATT table. The object can be instanciated by
+ * using the launch() function with the required arguments. The user is able to
+ * register a write callback that will be executed at the end of the write
+ * procedure with the final outcome of the operation.
+ *
+ * @note This object will automatically release any resources acquired during
+ *       its lifetime.
+ */
 class OneShotWriteCallback {
 public:
+    /**
+     * Create a new instance of the object and launch the write procedure with
+     * the specified arguments.
+     *
+     * @param[in] client
+     *              A pointer to the GattClient used to trigger the write
+     *              procedure.
+     * @param[in] connHandle
+     *              The ID of the connection to execute the write procedure.
+     * @param[in] handle
+     *              The attribute handle of the value written in the peer's
+     *              ATT table.
+     * @param[in] cb
+     *              The callback executed at the end of the write procedure.
+     */
     static void launch(GattClient* client, Gap::Handle_t connHandle,
                        GattAttribute::Handle_t handle, const GattClient::WriteCallback_t& cb) {
         OneShotWriteCallback* oneShot = new OneShotWriteCallback(client, connHandle, handle, cb);
         oneShot->attach();
-        // delete will be made when this callback is called
+        /* Delete will be made when this callback is called */
     }
 
 private:
+    /**
+     * Construct a new instance of this object.
+     *
+     * @param[in] client
+     *              A pointer to the GattClient used to trigger the write
+     *              procedure.
+     * @param[in] connHandle
+     *              The ID of the connection to execute the write procedure.
+     * @param[in] handle
+     *              The attribute handle of the value written in the peer's
+     *              ATT table.
+     * @param[in] cb
+     *              The callback executed at the end of the write procedure.
+     */
     OneShotWriteCallback(GattClient* client, Gap::Handle_t connHandle,
                         GattAttribute::Handle_t handle, const GattClient::WriteCallback_t& cb) :
         _client(client),
@@ -39,15 +78,30 @@ private:
         _handle(handle),
         _callback(cb) { }
 
-    void attach() {
+    /**
+     * Attach callbacks to the GattClient and Gap to receive shutdown, data
+     * written and disconnection events.
+     */
+    void attach(void) {
         _client->onDataWritten(makeFunctionPointer(this, &OneShotWriteCallback::call));
         _client->onShutdown(this, &OneShotWriteCallback::shutdownCallback);
         BLE::Instance().gap().onDisconnection(this, &OneShotWriteCallback::disconnectionCallback);
     }
 
+    /**
+     * This callback will be executed in case the user calls BLE::shutdown()
+     * before the write procedure completes. In this case, the call() callback
+     * will never be executed, but the resources acquired by this object need
+     * to be released.
+     *
+     * @param[in] client
+     *              A pointer to the GattClient used to trigger the write
+     *              procedure.
+     */
     void shutdownCallback(const GattClient *client) {
         /* Avoid compiler warnings */
         (void) client;
+
         /*
          * Free resources allocated to this object. Pending write callbacks
          * will no longer be executed.
@@ -55,6 +109,15 @@ private:
         delete this;
     }
 
+    /**
+     * This callback will be executed when a disconnection even occurs before
+     * the write procedure completes. In this case, the call() callback
+     * will never be executed, but the resources acquired by this object need
+     * to be released.
+     *
+     * @param[in] params
+     *              The disconnection parameters.
+     */
     void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params) {
         if (params->handle == _connHandle) {
             /* Tell the user that the operation failed */
@@ -78,8 +141,20 @@ private:
         }
     }
 
+    /**
+     * Registered callback that will be executed at the end of the write
+     * procedure. If this callback is called with the correct connection and
+     * attribute handles, then it will do the following:
+     * - Call the user-registered write callback.
+     * - Detach its callbacks registered for disconnection, shutdown and
+     *   data written events.
+     * - Release its resources.
+     *
+     * @param[in] param
+     *              Parameters specifying the result of the write operation.
+     */
     void call(const GattWriteCallbackParams* params) {
-        // verifiy that it is the right characteristic on the right connection
+        /* Verifiy that it is the right characteristic on the right connection */
         if (params->connHandle == _connHandle && params->handle == _handle) {
             _callback(params);
             _client->onDataWritten().detach(makeFunctionPointer(this, &OneShotWriteCallback::call));
@@ -89,9 +164,22 @@ private:
         }
     }
 
+    /**
+     * The GattClient instance that created this object.
+     */
     GattClient* _client;
+    /**
+     * The connection ID that used to execute the write procedure.
+     */
     Gap::Handle_t _connHandle;
+    /**
+     * The attribute handle of the value written in the peer's ATT table.
+     */
     GattAttribute::Handle_t _handle;
+    /**
+     * The registered callback that will be executed when the write procedure
+     * terminates.
+     */
     GattClient::WriteCallback_t _callback;
 };
 
