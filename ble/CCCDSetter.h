59c55c05734f724dba22257f57f309981c3191df
Andres Amaya Garcia <Andres.AmayaGarcia@arm.com>
2016-01-22 17:37:50 +0000

    Add documentation to CCCDSetter.h

--- ble/CCCDSetter.h
+++ ble/CCCDSetter.h
@@ -23,8 +23,35 @@
 #ifndef __CCCDSETTER_H__
 #define __CCCDSETTER_H__
 
+/**
+ * This class encapsulates the data needed to:
+ * - Discover Characteristic Descriptors
+ * - Identify the Client Configuration Characteristic Descriptor (CCCD) handle.
+ * - Intitiate the write procedure to the CCCD.
+ *
+ * The object can be instanciated by using the launch() function with the
+ * required arguments. The user is able to register a write callback that will
+ * be executed at the end of the CCCD write procedure with the final outcome of
+ * the operation.
+ *
+ * @note This object will automatically release any resources acquired during
+ *       its lifetime.
+ */
 class CCCDSetter {
 public:
+    /**
+     * Create a new instance of the object and launch the Characteristic
+     * Descriptor Discovery procedure to find the CCCD handle.
+     *
+     * @param[in] client
+     *              A pointer to the GattClient used for the operation.
+     * @param[in] characteristic
+     *              The characteristic that contains the CCCD to write.
+     * @param[in] callback
+     *              The callback executed at the end of the CCCD write procedure.
+     * @param[in] cccdData
+     *              The new data to write to the CCCD.
+     */
     static void launch(GattClient* client,
                        const DiscoveredCharacteristic &characteristic,
                        const GattClient::WriteCallback_t& callback,
@@ -34,6 +61,18 @@ public:
     }
 
 private:
+    /**
+     * Construct a new instance of this object.
+     *
+     * @param[in] client
+     *              A pointer to the GattClient used for the operation.
+     * @param[in] characteristic
+     *              The characteristic that contains the CCCD to write.
+     * @param[in] callback
+     *              The callback executed at the end of the CCCD write procedure.
+     * @param[in] cccdData
+     *              The new data to write to the CCCD.
+     */
     CCCDSetter(GattClient* client,
                const DiscoveredCharacteristic &characteristic,
                const GattClient::WriteCallback_t& callback,
@@ -44,6 +83,11 @@ private:
         _cccdData(cccdData),
         _cccdFound(false) { }
 
+    /**
+     * Start the Characteristic Descriptor Discovery procedure to find the CCCD
+     * handle and register callbacks for descriptor discovered and descriptor
+     * discovery termination events.
+     */
     void attach(void) {
         /* Launch descriptor discovery procedure */
         FunctionPointerWithContext<const CharacteristicDescriptorDiscovery::DiscoveryCallbackParams_t*> descriptorDiscoveredCallback(this, &CCCDSetter::descriptorDiscovered);
@@ -51,6 +95,14 @@ private:
         _characteristic.discoverDescriptors(descriptorDiscoveredCallback, descriptorDiscoveryTerminationCallback);
     }
 
+    /**
+     * Callback that will be executed when a characteristic descriptor discovery
+     * termination event occurs. If the CCCD was not previously found, then this
+     * callback will call the user-registered write callback with failure values.
+     *
+     * @note This callback release all resources previously acquired by this
+     *       object.
+     */
     void descriptorDiscoveryTermination(
         const CharacteristicDescriptorDiscovery::TerminationCallbackParams_t *) {
         /*
@@ -77,6 +129,15 @@ private:
         delete this;
     }
 
+    /**
+     * Callback that will be executed when a characteristic descriptor is
+     * discovered. If the descriptor is the CCCD, then the characteristic
+     * descriptor discovery procedure is terminated and a write procedure is
+     * started to update the CCCD.
+     *
+     * @param[in] params
+     *              Information about the discovered characteristric descriptor.
+     */
     void descriptorDiscovered(
         const CharacteristicDescriptorDiscovery::DiscoveryCallbackParams_t *params) {
         UUID cccdUuid(BLE_UUID_DESCRIPTOR_CLIENT_CHAR_CONFIG);
@@ -111,10 +172,26 @@ private:
         }
     }
 
+    /**
+     * The GattClient instance that created this object.
+     */
     GattClient*                 _client;
+    /**
+     * The discovered characteristic that contains the CCCD to write.
+     */
     DiscoveredCharacteristic    _characteristic;
+    /**
+     * The registered callback that will be executed when the write procedure
+     * terminates.
+     */
     GattClient::WriteCallback_t _callback;
+    /**
+     * The data to write to the CCCD.
+     */
     uint16_t                    _cccdData;
+    /**
+     * Flag when the CCCD has been found.
+     */
     bool                        _cccdFound;
 };
 
